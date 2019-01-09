34f50dd1fa2b7d83946079389abd58ce1f4f2b45
Yusuf <yusufksezer@hotmail.com>
2019-01-08 22:17:06 -0500

    linear pitch

--- spotify_visualizer.py
+++ spotify_visualizer.py
@@ -281,7 +281,7 @@ class SpotifyVisualizer:
                 interp1d(
                     start_times,
                     [pitch_list[i] if pitch_list[i] >= 0 else 0 for pitch_list in pitch_lists],
-                    kind="cubic",
+                    kind="linear",
                     assume_sorted=True
                 )
             )
